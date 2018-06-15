875d61c5afdc9a0b833bdd48bb14e03e5d4ebb24
vrakivnenko <vrakivnenko@griddynamics.com>
2018-06-15 17:00:56 -0500

    erg

---
+++ vars/win.groovy
@@ -0,0 +1,24 @@
+#!/usr/bin/env groovy
+def call (int numb) {
+        if (numb % 2 == 0) {
+                pipeline {
+                        agent any
+                        stages {
+                                stage('E stage') {
+                                        steps {
+                                                echo "number is even"
+                                        }
+                                }
+                        }
+                }
+        } else {
+                pipeline {
+                        agent any
+                        stages {
+                                stage ('o stage') {
+                                        steps {
+                                                echo "number is odd"
+                                        }
+                                }
+                        }
+                }
