c09cd518d1ba98b42657421f49a7624db6b6d9f5
vrakivnenko <vrakivnenko@griddynamics.com>
2018-06-15 15:12:47 -0500

    my lib

---
+++ lib/vars/new.groovy
@@ -0,0 +1,23 @@
+def new(int numb) {
+        if (numb % 2 == 0) {
+                pipeline {
+                        agent any
+                        stages {
+                                stage('E stage') {
+                                        steps {
+                                                echo "number is even"
+                                        }
+                                }
+                        }
+                }
+        } else {
+                pipeline {
+                        agent any
+                        stages {
+                                stage ('o stage') {
+                                        steps {
+                                                echo "number is odd"
+                                        }
+                                }
+                        }
+                }
