4c47d08f033ca91e97ff795dc8748eb36814d217
Vladislav Rakivnenko <vrakivnenko@vrakivnenko-it-us-macbook-pro.local>
2018-06-22 15:45:55 -0500

    fheb

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,15 +1,15 @@
 #!/usr/bin/env groovy
 def call () {
-	sh "ls -l"
-	def err = sh "./test.sh &> $(date +'%d_%m_%Y_%H_%M').log"
-	if (err) {
-		sh "echo 'Nice job'"
-		res="0" 
-	}
-	else {
-		sh "echo 'NOPE'"
-		res="3"
-	}
-	return res
+	sh "echo 'test.groovy'; ls -l"
+	// def err = sh "./test.sh &> $(date +'%d_%m_%Y_%H_%M').log"
+	// if (err) {
+	// 	sh "echo 'Nice job'"
+	// 	res="0" 
+	// }
+	// else {
+	// 	sh "echo 'NOPE'"
+	// 	res="3"
+	// }
+	// return res
 }
 	
