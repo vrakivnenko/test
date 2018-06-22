220ce33d325fe54e06b76897aded48609a27178b
Vladislav Rakivnenko <vrakivnenko@vrakivnenko-it-us-macbook-pro.local>
2018-06-22 16:33:25 -0500

    fhe

--- vars/test.groovy
+++ vars/test.groovy
@@ -8,7 +8,7 @@ def call () {
 	writeFile file: 'test_lib.sh', text: "libraryResource 'test.sh'"
 	sh "set -x; cat test_lib.sh"
 
-	def err = sh "test_lib.sh &> $(date +'%d_%m_%Y_%H_%M').log"
+	// def err = sh "test_lib.sh &> $(date +'%d_%m_%Y_%H_%M').log"
 	// if (err) {
 	// 	sh "echo 'Nice job'"
 	// 	res="0" 
