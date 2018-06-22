3334a295cbb778f14dc23295c87f4b7ea470d9ac
Vladislav Rakivnenko <vrakivnenko@vrakivnenko-it-us-macbook-pro.local>
2018-06-22 17:25:59 -0500

    fhe

--- vars/test.groovy
+++ vars/test.groovy
@@ -2,7 +2,7 @@
 def call () {
 	def script_content = libraryResource 'test.sh'
 	writeFile(file: 'test_lib.sh', text: "${script_content}")
-	sh "set -x; chmod a+x test_lib.sh; ls -l; cat test_lib.sh;"
+	sh "chmod a+x test_lib.sh"
 	def err = sh(script: "./test_lib.sh ", returnStatus: true)
 	println(err)
 }
