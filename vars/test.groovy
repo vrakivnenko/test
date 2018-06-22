4ae8d0457a497b24ec9db2ddb5e18653f221439d
Vladislav Rakivnenko <vrakivnenko@vrakivnenko-it-us-macbook-pro.local>
2018-06-22 16:44:22 -0500

    fhe

--- vars/test.groovy
+++ vars/test.groovy
@@ -5,8 +5,8 @@ def call () {
 
 	// sh "set -x; echo \"${script_content}\" > test_lib.sh && chmod a+x test_lib.sh && ./test_lib.sh"
 
-	writeFile file: 'test_lib.sh', text: "libraryResource 'test.sh'"
-	sh "set -x; cat test_lib.sh"
+	// writeFile file: 'test_lib.sh', text: "libraryResource 'test.sh'"
+	sh "set -x; cat test_lib.sh; chmod a+x test_lib.sh"
 
 	def err = sh "./test_lib.sh" 
 	// &> $(date +'%d_%m_%Y_%H_%M').log"
