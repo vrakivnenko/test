6e4a49d5c3d72a26342892454a1ec0240e1938d0
Vladislav Rakivnenko <vrakivnenko@vrakivnenko-it-us-macbook-pro.local>
2018-06-26 11:37:16 -0500

    new script

--- vars/py_check.groovy
+++ vars/py_check.groovy
@@ -1,16 +1,17 @@
 #!/usr/bin/env groovy
 def call () {
+
     echo "check"
-    // echo $check_file
-	// def score = sh "pylint -d C,R $check_file 2>dev/null | grep rate | awk '{print $7}' | awk -F \"/\" '{print $1}' | awk -F \".\" '{print $1}'"
-    // if (score > 8) {
-    //     println "your code is exellent"
-    //     return "0"
-    // } else if (score > 0) {
-    //     println "your code good enough, but can be better"
-    //     return "0"
-    // } else {
-    //     println "you need to work on your code it`s terrible"
-    //     return "1"
-    // }
+    echo $check_file
+	def score = sh "pylint -d C,R $check_file 2>dev/null | grep rate | awk '{print $7}' | awk -F \"/\" '{print $1}' | awk -F \".\" '{print $1}'"
+    if (score > 8) {
+        echo "your code is exellent"
+        return "0"
+    } else if (score > 0) {
+        echo "your code good enough, but can be better"
+        return "0"
+    } else {
+        echo "you need to work on your code it`s terrible"
+        return "1"
+    }
 }
