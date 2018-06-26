e5afcd893c23dafa68ecb51af05871825dac9442
Vladislav Rakivnenko <vrakivnenko@vrakivnenko-it-us-macbook-pro.local>
2018-06-26 17:39:42 -0500

    add some description

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,6 +1,7 @@
 #!/usr/bin/env groovy
 def call () {
 	def script_content = libraryResource 'test.sh'
+	//creating file to be able to run script
 	writeFile(file: 'test_lib.sh', text: "${script_content}")
 	sh "chmod a+x test_lib.sh"
 	def err = sh(script: "./test_lib.sh ", returnStatus: true)
