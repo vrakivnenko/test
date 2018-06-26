9f04e4dd48075a13519c330efee57363691cac7a
Vladislav Rakivnenko <vrakivnenko@vrakivnenko-it-us-macbook-pro.local>
2018-06-26 12:52:57 -0500

    making it right

--- vars/py_check.groovy
+++ vars/py_check.groovy
@@ -3,14 +3,15 @@ def call (check) {
 
     println check
 	def score = sh(script: "pylint -d C,R $check 2> /dev/null | grep rate | awk '{print $7}' | awk -F \"/\" '{print $1}' | awk -F \".\" '{print $1}'", returnStdout: true)
-    if (score.toInteger() > 8) {
-        echo "your code is exellent"
-        return "0"
-    // } else if (score.toInteger() > 0) {
-    //     echo "your code good enough, but can be better"
+    println score
+    // if (score.toInteger() > 8) {
+    //     echo "your code is exellent"
     //     return "0"
-    } else {
-        echo "you need to work on your code it`s terrible"
-        return "1"
-    }
+    // // } else if (score.toInteger() > 0) {
+    // //     echo "your code good enough, but can be better"
+    // //     return "0"
+    // } else {
+    //     echo "you need to work on your code it`s terrible"
+    //     return "1"
+    // }
 }
