63d0090ca8d350b8dd0b99de34ebdf64395d3bdd
Vladislav Rakivnenko <vrakivnenko@vrakivnenko-it-us-macbook-pro.local>
2018-06-26 10:53:17 -0500

    new library

--- vars/py_check.groovy
+++ vars/py_check.groovy
@@ -3,9 +3,12 @@ def call () {
 	def score = sh "pylint -d C,R $check_file 2>dev/null | grep rate | awk '{print $7}' | awk -F \"/\" '{print $1}' | awk -F \".\" '{print $1}'"
     if (score > 8) {
         println "your code is exellent"
+        return "0"
     } else if (score > 0) {
         println "your code good enough, but can be better"
+        return "0"
     } else {
         println "you need to work on your code it`s terrible"
+        return "1"
     }
 }
